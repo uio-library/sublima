e1bc9e2672497aac150db56da29f66537225b22c
kkj <kkj@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-01-31 12:22:16 +0000

    Working dispatcher
    git-svn-id: https://submarine.computas.com/sublima/trunk@227 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparqlDispatcher.java
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparqlDispatcher.java
@@ -1,11 +1,43 @@
 package com.computas.sublima.query.impl;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.apache.cocoon.configuration.Settings;
+import org.apache.commons.io.IOUtils;
+
 import com.computas.sublima.query.SparqlDispatcher;
 
+/**
+ * This component queries RDF triple stores using Sparql. It is threadsafe.
+ */
 public class DefaultSparqlDispatcher implements SparqlDispatcher {
 
+	private Settings cocoonSettings;
+
 	public Object query() {
-	    return "result of a sparql query";	
+		String result = null;
+		try {
+			String url = cocoonSettings
+					.getProperty("com.computas.sublima.query.joseki.endpoint");
+
+			URL u = new URL(url
+					+ "?query="
+					+ URLEncoder.encode("DESCRIBE <http://the-jet.com/>",
+							"UTF-8"));
+
+			HttpURLConnection con = (HttpURLConnection) u.openConnection();
+
+			result = IOUtils.toString(con.getInputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public void setCocoonSettings(Settings cocoonSettings) {
+		this.cocoonSettings = cocoonSettings;
 	}
 
 }
--- blocks/sublima-query/src/main/resources/COB-INF/flow/query.js
+++ blocks/sublima-query/src/main/resources/COB-INF/flow/query.js
@@ -1,7 +1,7 @@
 function query() {
   var sparqlDispatcher = cocoon.getComponent("com.computas.sublima.query.SparqlDispatcher");
   var result = sparqlDispatcher.query();
-  cocoon.sendPage("screens/result-list", 
+  cocoon.sendPage("rdf/result-list", 
     {
       "result" : result
     }
