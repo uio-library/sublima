4634ed37c2aab87683a6fba0d1b5f3f051faa9cf
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-02-06 11:16:08 +0000

    Changed the SPARQL, no WWW!
    git-svn-id: https://submarine.computas.com/sublima/trunk@250 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/transform/NavigationAndFacetsTransformer.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/transform/NavigationAndFacetsTransformer.java
@@ -1,8 +1,6 @@
 package com.computas.sublima.transform;
 
-import java.io.IOException;
-import java.util.Map;
-
+import com.computas.sublima.query.SparqlDispatcher;
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.environment.SourceResolver;
@@ -12,53 +10,54 @@ import org.apache.cocoon.xml.StringXMLizable;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
-import com.computas.sublima.query.SparqlDispatcher;
+import java.io.IOException;
+import java.util.Map;
 
 public class NavigationAndFacetsTransformer extends AbstractTransformer {
 
-	private SparqlDispatcher sparqlDispatcher;
-	
-	public void setup(SourceResolver resolver, Map objectModel, String src,
-			Parameters par) throws ProcessingException, SAXException,
-			IOException {
+  private SparqlDispatcher sparqlDispatcher;
 
-	}
+  public void setup(SourceResolver resolver, Map objectModel, String src,
+                    Parameters par) throws ProcessingException, SAXException,
+          IOException {
 
-	@Override
-	public void startElement(String uri, String loc, String raw, Attributes a)
-			throws SAXException {
+  }
 
-        if ("navigation".equals(loc)) {
-			super.startElement(uri, loc, raw, a);
+  @Override
+  public void startElement(String uri, String loc, String raw, Attributes a)
+          throws SAXException {
 
-            //todo Use the existing SPARQL resultset and create a query that returns the navigation elements
-            String result = (String) this.sparqlDispatcher.query("DESCRIBE <http://www.the-jet.com/>");
-			StringXMLizable stringXMLizable = new StringXMLizable(result);
-			stringXMLizable.toSAX(new IncludeXMLConsumer(this.contentHandler));
-            return;
-		}
+    if ("navigation".equals(loc)) {
+      super.startElement(uri, loc, raw, a);
 
-        if ("facets".equals(loc)) {
-			super.startElement(uri, loc, raw, a);
+      //todo Use the existing SPARQL resultset and create a query that returns the navigation elements
+      String result = (String) this.sparqlDispatcher.query("DESCRIBE <http://the-jet.com/>");
+      StringXMLizable stringXMLizable = new StringXMLizable(result);
+      stringXMLizable.toSAX(new IncludeXMLConsumer(this.contentHandler));
+      return;
+    }
 
-            //todo Use the existing SPARQL resultset and create a query that returns the facets
-            String result = (String) this.sparqlDispatcher.query("DESCRIBE <http://www.the-jet.com/>");
-			StringXMLizable stringXMLizable = new StringXMLizable(result);
-			stringXMLizable.toSAX(new IncludeXMLConsumer(this.contentHandler));
-            return;
-		}
+    if ("facets".equals(loc)) {
+      super.startElement(uri, loc, raw, a);
 
-        super.startElement(uri, loc, raw, a);
-	}
-	
-	@Override
-	public void endElement(String uri, String loc, String raw) throws SAXException {
+      //todo Use the existing SPARQL resultset and create a query that returns the facets
+      String result = (String) this.sparqlDispatcher.query("DESCRIBE <http://the-jet.com/>");
+      StringXMLizable stringXMLizable = new StringXMLizable(result);
+      stringXMLizable.toSAX(new IncludeXMLConsumer(this.contentHandler));
+      return;
+    }
 
-		super.endElement(uri, loc, raw);
-	}
+    super.startElement(uri, loc, raw, a);
+  }
 
-	public void setSparqlDispatcher(SparqlDispatcher sparqlDispatcher) {
-		this.sparqlDispatcher = sparqlDispatcher;
-	}
+  @Override
+  public void endElement(String uri, String loc, String raw) throws SAXException {
+
+    super.endElement(uri, loc, raw);
+  }
+
+  public void setSparqlDispatcher(SparqlDispatcher sparqlDispatcher) {
+    this.sparqlDispatcher = sparqlDispatcher;
+  }
 
 }
