e6efc56d738e023cb361c8dfb5560f7dcb0b311c
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-02-06 14:17:00 +0000

    Construct to get the Navigation elements. weeeee!
    git-svn-id: https://submarine.computas.com/sublima/trunk@253 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/transform/NavigationAndFacetsTransformer.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/transform/NavigationAndFacetsTransformer.java
@@ -16,22 +16,62 @@ import java.util.Map;
 public class NavigationAndFacetsTransformer extends AbstractTransformer {
 
   private SparqlDispatcher sparqlDispatcher;
+  String subject;
 
   public void setup(SourceResolver resolver, Map objectModel, String src,
                     Parameters par) throws ProcessingException, SAXException,
           IOException {
-
   }
 
   @Override
   public void startElement(String uri, String loc, String raw, Attributes a)
           throws SAXException {
 
+    if ("dc:subject".equals(raw)) {
+      subject = "<" + a.getValue("rdf:resource") + ">";
+    }
+
     if ("navigation".equals(loc)) {
       super.startElement(uri, loc, raw, a);
 
       //todo Use the existing SPARQL resultset and create a query that returns the navigation elements
-      String result = (String) this.sparqlDispatcher.query("DESCRIBE <http://the-jet.com/>");
+
+      String sparqlConstructQuery =
+              "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+              "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+              "prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
+              "prefix foaf: <http://xmlns.com/foaf/0.1/>\n" +
+              "prefix lingvoj: <http://www.lingvoj.org/ontology#>\n" +
+              "prefix dcmitype: <http://purl.org/dc/dcmitype/>\n" +
+              "prefix dct: <http://purl.org/dc/terms/>\n" +
+              "prefix sub: <http://xmlns.computas.com/sublima#>\n" +
+              "prefix wdr: <http://www.w3.org/2007/05/powder#>\n" +
+              "prefix sioc: <http://rdfs.org/sioc/ns#>\n" +
+              "prefix skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+              "CONSTRUCT {\n" +
+                      subject + " skos:prefLabel ?label ; \n" +
+                      " skos:altLabel ?synLabel ;\n" +
+                      " skos:related ?relSub ;\n" +
+                      " skos:broader ?btSub ;\n" +
+                      " skos:narrower ?ntSub .\n" +
+                      " ?relSub skos:prefLabel ?relLabel .\n" +
+                      " ?btSub skos:prefLabel ?btLabel .\n" +
+                      " ?ntSub skos:prefLabel ?ntLabel .\n" +
+                      " }\n" +
+                      " WHERE {\n" +
+                      subject + " rdfs:label ?label .\n" +
+                      subject + " a ?class .\n" +
+                      " OPTIONAL { " + subject + " <http://xmlns.computas.com/sublima#synonym> ?synLabel  . }\n" +
+                      " OPTIONAL { " + subject + " ?prop ?relSub .\n" +
+                      " ?relSub rdfs:label ?relLabel . }\n" +
+                      " OPTIONAL { ?class rdfs:subClassOf ?btClass .\n" +
+                      " ?btSub a ?btClass ;\n" +
+                      " rdfs:label ?btLabel . }\n" +
+                      " OPTIONAL { ?ntClass rdfs:subClassOf ?class .\n" +
+                      " ?ntSub a ?ntClass .\n" +
+                      " ?ntClass rdfs:label ?ntLabel . } }";
+
+      String result = (String) this.sparqlDispatcher.query(sparqlConstructQuery);
       StringXMLizable stringXMLizable = new StringXMLizable(result);
       stringXMLizable.toSAX(new IncludeXMLConsumer(this.contentHandler));
       return;
