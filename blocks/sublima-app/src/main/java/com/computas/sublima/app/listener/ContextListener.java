0bb1bbfe8574a0e82c8ad78e9cf3f37e5835bed2
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-02-29 07:01:23 +0000

    Added logging to the ContextListener that perform the indexing
    git-svn-id: https://submarine.computas.com/sublima/trunk@323 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/listener/ContextListener.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/listener/ContextListener.java
@@ -10,34 +10,43 @@ import com.hp.hpl.jena.query.larq.LARQ;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletContextEvent;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author: mha
  * Date: 28.feb.2008
  */
 public final class ContextListener implements ServletContextListener {
 
+  private static Logger logger = Logger.getLogger(ContextListener.class);
+
   public void contextInitialized(ServletContextEvent servletContextEvent) {
     DatabaseService myDbService = new DatabaseService();
     IDBConnection connection = myDbService.getConnection();
+    logger.info("INDEXING ------- Created database connection " + connection.getDatabaseType());
 
     // -- Read and index all literal strings.
     IndexBuilderString larqBuilder = new IndexBuilderString();
+    logger.info("INDEXING ------- Read and indexed all literal strings");
 
     //IndexBuilderSubject larqBuilder = new IndexBuilderSubject();
 
     //Create a model based on the one in the DB
     ModelRDB model = ModelRDB.open(connection);
+    logger.info("INDEXING ------- Created RDF model from database");
 
     // -- Create an index based on existing statements
     larqBuilder.indexStatements(model.listStatements());
+    logger.info("INDEXING ------- Indexed all model statements");
     // -- Finish indexing
     larqBuilder.closeForWriting();
+    logger.info("INDEXING ------- Closed index for writing");
     // -- Create the access index
     IndexLARQ index = larqBuilder.getIndex();
-    System.out.println("LARQ ----------- CREATE INDEX");
 
     // -- Make globally available
     LARQ.setDefaultIndex(index);
+    logger.info("INDEXING ------- Index now globally available");
   }
 
   public void contextDestroyed(ServletContextEvent servletContextEvent) {
