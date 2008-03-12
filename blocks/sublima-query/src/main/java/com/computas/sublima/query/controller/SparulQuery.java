614a6ee6e73f3803fb31e4c1db1b366ce67763da
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-03-12 09:07:24 +0000

    Sparql Update
    git-svn-id: https://submarine.computas.com/sublima/trunk@339 8fc9d99b-3020-0410-9de1-dfa816a989ce

---
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/controller/SparulQuery.java
@@ -0,0 +1,32 @@
+package com.computas.sublima.query.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.components.flow.apples.AppleRequest;
+import org.apache.cocoon.components.flow.apples.AppleResponse;
+import org.apache.cocoon.components.flow.apples.StatelessAppleController;
+
+import com.computas.sublima.query.SparulDispatcher;
+
+public class SparulQuery implements StatelessAppleController {
+
+	private SparulDispatcher sparulDispatcher;
+
+	public void process(AppleRequest req, AppleResponse res) throws Exception {
+		String sparqlQuery = req.getCocoonRequest().getParameter("insert");
+		if(sparqlQuery == null || "".equals(sparqlQuery)) {
+			throw new ProcessingException("A SPARUL insert string has to be passed.");
+		}
+		Object queryResult = sparulDispatcher.query(sparqlQuery);
+		Map<String, Object> bizData = new HashMap<String, Object>();
+		bizData.put("result", queryResult);
+		res.sendPage("rdf/result-list", bizData);
+	}
+
+	public void setSparqlDispatcher(SparulDispatcher sparulDispatcher) {
+		this.sparulDispatcher = sparulDispatcher;
+	}
+
+}
