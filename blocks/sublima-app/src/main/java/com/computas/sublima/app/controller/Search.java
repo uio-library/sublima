acb485e519a66060d99d154718b7f67e1291b6b1
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-03-12 09:04:42 +0000

    Introduced a default boolean operator
    git-svn-id: https://submarine.computas.com/sublima/trunk@335 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/Search.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/Search.java
@@ -1,38 +1,38 @@
 package com.computas.sublima.app.controller;
 
 import com.computas.sublima.app.Form2SparqlService;
+import com.computas.sublima.app.SearchService;
 import com.computas.sublima.query.SparqlDispatcher;
 import com.computas.sublima.query.SparulDispatcher;
 import com.computas.sublima.query.service.DatabaseService;
 import com.hp.hpl.jena.db.IDBConnection;
 import com.hp.hpl.jena.db.ModelRDB;
-import com.hp.hpl.jena.query.*;
-import com.hp.hpl.jena.query.larq.IndexBuilderSubject;
-import com.hp.hpl.jena.query.larq.IndexLARQ;
-import com.hp.hpl.jena.query.larq.LARQ;
-import com.hp.hpl.jena.query.larq.IndexBuilderString;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.sparql.util.StringUtils;
 import org.apache.cocoon.components.flow.apples.AppleRequest;
 import org.apache.cocoon.components.flow.apples.AppleResponse;
 import org.apache.cocoon.components.flow.apples.StatelessAppleController;
+import org.apache.cocoon.configuration.Settings;
 import org.apache.cocoon.environment.Request;
 import org.apache.log4j.Logger;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.io.*;
 
 public class Search implements StatelessAppleController {
-
+  private Settings cocoonSettings;
   private SparqlDispatcher sparqlDispatcher;
   private SparulDispatcher sparulDispatcher;
   private String mode;
 
   private static Logger logger = Logger.getLogger(Search.class);
-
 
   @SuppressWarnings("unchecked")
   public void process(AppleRequest req, AppleResponse res) throws Exception {
@@ -55,57 +55,55 @@ public class Search implements StatelessAppleController {
       doFreeTextSearch(res, req);
       return;
     }
-    
+
     if ("topic-instance".equalsIgnoreCase(mode)) {
       doGetTopic(res, req);
       return;
     }
-
-
   }
 
   private void doGetTopic(AppleResponse res, AppleRequest req) {
-	    DatabaseService myDbService = new DatabaseService();
-	    IDBConnection connection = myDbService.getConnection();
-	    
-	    //Create a model based on the one in the DB
-	    ModelRDB model = ModelRDB.open(connection);
-	    String subject = "http://sublima.computas.com/topic-instance/"+req.getSitemapParameter("topic");
-	    String queryString = StringUtils.join("\n", new String[] {
-	            "PREFIX dct: <http://purl.org/dc/terms/>",
-	            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
-	            "DESCRIBE ?resource <"+subject+"> ?publisher",
-	            "WHERE {",
-	            "        ?resource dct:language ?lang;",
-	            "				 dct:publisher ?publisher ;",
-	            "                dct:subject <"+subject+"> .}"});
-	    System.out.println(queryString);
-	    Query query = QueryFactory.create(queryString);
-	    QueryExecution qExec = QueryExecutionFactory.create(query, model);
-	    Model m = qExec.execDescribe();
-	    qExec.close();
-	    ByteArrayOutputStream bout = new ByteArrayOutputStream();
-	    m.write(bout, "RDF/XML-ABBREV");
+    DatabaseService myDbService = new DatabaseService();
+    IDBConnection connection = myDbService.getConnection();
 
+    //Create a model based on the one in the DB
+    ModelRDB model = ModelRDB.open(connection);
+    String subject = "http://sublima.computas.com/topic-instance/" + req.getSitemapParameter("topic");
+    String queryString = StringUtils.join("\n", new String[]{
+            "PREFIX dct: <http://purl.org/dc/terms/>",
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+            "DESCRIBE ?resource <" + subject + "> ?publisher",
+            "WHERE {",
+            "        ?resource dct:language ?lang;",
+            "				 dct:publisher ?publisher ;",
+            "                dct:subject <" + subject + "> .}"});
+    System.out.println(queryString);
+    Query query = QueryFactory.create(queryString);
+    QueryExecution qExec = QueryExecutionFactory.create(query, model);
+    Model m = qExec.execDescribe();
+    qExec.close();
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    m.write(bout, "RDF/XML-ABBREV");
 
-	    Map<String, Object> bizData = new HashMap<String, Object>();
-	    bizData.put("result-list", bout.toString());
-	    bizData.put("mode", mode);
-	    res.sendPage("xml/sparql-result", bizData);
+    Map<String, Object> bizData = new HashMap<String, Object>();
+    bizData.put("result-list", bout.toString());
+    bizData.put("mode", mode);
+    res.sendPage("xml/sparql-result", bizData);
   }
 
-  
-  private void doFreeTextSearch(AppleResponse res, AppleRequest req) {
-    String searchstring = req.getCocoonRequest().getParameter("searchstring");
 
+  private void doFreeTextSearch(AppleResponse res, AppleRequest req) {
+    String defaultBooleanOperator = cocoonSettings.getProperty("sublima.default.boolean.operator");
+    SearchService searchService = new SearchService(defaultBooleanOperator);
+    String searchstring = searchService.buildSearchString(req.getCocoonRequest().getParameter("searchstring"));
 
     DatabaseService myDbService = new DatabaseService();
     IDBConnection connection = myDbService.getConnection();
-    
+
     //Create a model based on the one in the DB
     ModelRDB model = ModelRDB.open(connection);
 
-    String queryString = StringUtils.join("\n", new String[] {
+    String queryString = StringUtils.join("\n", new String[]{
             "PREFIX pf: <http://jena.hpl.hp.com/ARQ/property#>",
             "PREFIX dct: <http://purl.org/dc/terms/>",
             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
@@ -123,9 +121,9 @@ public class Search implements StatelessAppleController {
     QueryExecution qExec = QueryExecutionFactory.create(query, model);
     Model m = qExec.execDescribe();
     qExec.close();
+
     ByteArrayOutputStream bout = new ByteArrayOutputStream();
     m.write(bout, "RDF/XML-ABBREV");
-
 
     Map<String, Object> bizData = new HashMap<String, Object>();
     bizData.put("result-list", bout.toString());
@@ -171,14 +169,6 @@ public class Search implements StatelessAppleController {
     res.sendPage("xml/sparql-result", bizData);
   }
 
-  public void setSparqlDispatcher(SparqlDispatcher sparqlDispatcher) {
-    this.sparqlDispatcher = sparqlDispatcher;
-  }
-
-  public void setSparulDispatcher(SparulDispatcher sparulDispatcher) {
-    this.sparulDispatcher = sparulDispatcher;
-  }
-
   private Map<String, String[]> createParametersMap(Request request) {
     Map<String, String[]> result = new HashMap<String, String[]>();
     Enumeration parameterNames = request.getParameterNames();
@@ -189,4 +179,15 @@ public class Search implements StatelessAppleController {
     return result;
   }
 
+  public void setSparqlDispatcher(SparqlDispatcher sparqlDispatcher) {
+    this.sparqlDispatcher = sparqlDispatcher;
+  }
+
+  public void setSparulDispatcher(SparulDispatcher sparulDispatcher) {
+    this.sparulDispatcher = sparulDispatcher;
+  }
+
+  public void setCocoonSettings(Settings cocoonSettings) {
+    this.cocoonSettings = cocoonSettings;
+  }
 }
