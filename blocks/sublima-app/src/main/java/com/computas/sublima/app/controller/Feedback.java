f2107f6282d18c6f93c3c68be10eaa935a0fb0d8
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-03-25 14:15:27 +0000

    Added keywords (inserted as sub:keyword)
    git-svn-id: https://submarine.computas.com/sublima/trunk@383 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/Feedback.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/Feedback.java
@@ -31,6 +31,7 @@ public class Feedback implements StatelessAppleController {
       String url = req.getCocoonRequest().getParameter("url");
       String tittel = req.getCocoonRequest().getParameter("tittel");
       String beskrivelse = req.getCocoonRequest().getParameter("beskrivelse");
+      String[] stikkord = req.getCocoonRequest().getParameter("stikkord").split(","); 
 
       // Do a URL check so that we know we have a valid URL
       IndexService indexService = new IndexService();
@@ -38,17 +39,26 @@ public class Feedback implements StatelessAppleController {
 
       //todo We have to get the interface-language @no from somewhere
       if (status == 200) {
-        String updateString = StringUtils.join("\n", new String[]{
-                "PREFIX dct: <http://purl.org/dc/terms/>",
-                "PREFIX wdr: <http://www.w3.org/2007/05/powder#>",
-                "INSERT",
-                "{",
-                "<" + url + ">" + " dct:title " + "\"" + tittel + "\"@no ; ",
-                "dct:description " + "\"" + beskrivelse + "\"@no ;",
-                "wdr:describedBy <http://sublima.computas.com/status/til_godkjenning> .",
-                "}"});
+        String partialUpdateString =
+                "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "PREFIX wdr: <http://www.w3.org/2007/05/powder#>\n" +
+                "PREFIX sub: <http://xmlns.computas.com/sublima#>\n" +
+                "INSERT\n" +
+                "{\n" +
+                "<" + url + ">" + " dct:title " + "\"" + tittel + "\"@no ; \n" +
+                "dct:description " + "\"" + beskrivelse + "\"@no ; \n" +
+                "wdr:describedBy <http://sublima.computas.com/status/til_godkjenning> ;\n";
+        
+        StringBuffer partialUpdateStringBuffer = new StringBuffer();
 
-        success = sparulDispatcher.query(updateString);
+        partialUpdateStringBuffer.append(partialUpdateString);
+        //for each keyword, add a sub:keyword
+        for(String s : stikkord) {
+          partialUpdateStringBuffer.append("sub:keyword \"" + s + "\"@no ;\n");
+        }
+        partialUpdateStringBuffer.append("}");
+
+        success = sparulDispatcher.query(partialUpdateStringBuffer.toString());
 
         if (success) {
           res.sendPage("takk", null);
