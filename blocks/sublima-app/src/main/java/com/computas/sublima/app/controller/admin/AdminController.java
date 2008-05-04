d31c9c20c4979d9dc8600bdd55f7f9641759f47c
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-05-04 20:35:41 +0000

    git-svn-id: https://submarine.computas.com/sublima/trunk@594 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/admin/AdminController.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/admin/AdminController.java
@@ -21,82 +21,79 @@ import java.util.*;
  */
 public class AdminController implements StatelessAppleController {
 
-  private SparqlDispatcher sparqlDispatcher;
-  private SparulDispatcher sparulDispatcher;
-  AdminService adminService = new AdminService();
-  private String mode;
-  private String submode;
-  String[] completePrefixArray = {
-          "PREFIX dct: <http://purl.org/dc/terms/>",
-          "PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
-          "PREFIX sub: <http://xmlns.computas.com/sublima#>",
-          "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
-          "PREFIX wdr: <http://www.w3.org/2007/05/powder#>",
-          "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>",
-          "PREFIX lingvoj: <http://www.lingvoj.org/ontology#>"};
+    private SparqlDispatcher sparqlDispatcher;
+    private SparulDispatcher sparulDispatcher;
+    AdminService adminService = new AdminService();
+    private String mode;
+    private String submode;
+    String[] completePrefixArray = {
+            "PREFIX dct: <http://purl.org/dc/terms/>",
+            "PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
+            "PREFIX sub: <http://xmlns.computas.com/sublima#>",
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+            "PREFIX wdr: <http://www.w3.org/2007/05/powder#>",
+            "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>",
+            "PREFIX lingvoj: <http://www.lingvoj.org/ontology#>"};
 
-  String completePrefixes = StringUtils.join("\n", completePrefixArray);
-  String[] prefixArray = {
-          "dct: <http://purl.org/dc/terms/>",
-          "foaf: <http://xmlns.com/foaf/0.1/>",
-          "sub: <http://xmlns.computas.com/sublima#>",
-          "rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
-          "wdr: <http://www.w3.org/2007/05/powder#>",
-          "skos: <http://www.w3.org/2004/02/skos/core#>",
-          "PREFIX lingvoj: <http://www.lingvoj.org/ontology#>"};
-  String prefixes = StringUtils.join("\n", prefixArray);
+    String completePrefixes = StringUtils.join("\n", completePrefixArray);
+    String[] prefixArray = {
+            "dct: <http://purl.org/dc/terms/>",
+            "foaf: <http://xmlns.com/foaf/0.1/>",
+            "sub: <http://xmlns.computas.com/sublima#>",
+            "rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+            "wdr: <http://www.w3.org/2007/05/powder#>",
+            "skos: <http://www.w3.org/2004/02/skos/core#>",
+            "PREFIX lingvoj: <http://www.lingvoj.org/ontology#>"};
+    String prefixes = StringUtils.join("\n", prefixArray);
 
-  private static Logger logger = Logger.getLogger(AdminController.class);
+    private static Logger logger = Logger.getLogger(AdminController.class);
 
-  @SuppressWarnings("unchecked")
-  public void process(AppleRequest req, AppleResponse res) throws Exception {
+    @SuppressWarnings("unchecked")
+    public void process(AppleRequest req, AppleResponse res) throws Exception {
 
-    this.mode = req.getSitemapParameter("mode");
-    this.submode = req.getSitemapParameter("submode");
+        this.mode = req.getSitemapParameter("mode");
+        this.submode = req.getSitemapParameter("submode");
 
-    if ("".equalsIgnoreCase(mode)) {
-      res.sendPage("xml2/admin", null);
-    } else if ("testsparql".equalsIgnoreCase(mode)) {
-      if ("".equalsIgnoreCase(submode)) {
-        res.sendPage("xhtml/testsparql", null);
-      } else {
-        String query = req.getCocoonRequest().getParameter("query");
-        res.redirectTo(req.getCocoonRequest().getContextPath() + "/sparql?query=" + URLEncoder.encode(query, "UTF-8"));
-      }
+        if ("".equalsIgnoreCase(mode)) {
+            res.sendPage("xml2/admin", null);
+        } else if ("testsparql".equalsIgnoreCase(mode)) {
+            if ("".equalsIgnoreCase(submode)) {
+                res.sendPage("xhtml/testsparql", null);
+            } else {
+                String query = req.getCocoonRequest().getParameter("query");
+                res.redirectTo(req.getCocoonRequest().getContextPath() + "/sparql?query=" + URLEncoder.encode(query, "UTF-8"));
+            }
+        } else if ("database".equalsIgnoreCase(mode)) {
+            uploadForm(res, req);
+        } else {
+            res.sendStatus(404);
+            return;
+        }
     }
 
-    // Linkcheck. Send the user to a page that displays a list of all resources affected.
-    else if ("database".equalsIgnoreCase(mode)) {
-      uploadForm(res, req);
-    } else {
-      res.sendStatus(404);
-      return;
+    private void uploadForm(AppleResponse res, AppleRequest req) {
+        if (req.getCocoonRequest().getMethod().equalsIgnoreCase("GET")) {
+            res.sendPage("xml2/upload", null);
+        } else if (req.getCocoonRequest().getMethod().equalsIgnoreCase("POST")) {
+            String location = req.getCocoonRequest().getParameter("location");
+            String type = req.getCocoonRequest().getParameter("type");
+
+            ImportData importData = new ImportData();
+            importData.load(location, type);
+            res.sendPage("xml2/upload", null);
+        }
     }
-  }
 
-  private void uploadForm(AppleResponse res, AppleRequest req) {
-    if (req.getCocoonRequest().getMethod().equalsIgnoreCase("GET")) {
-      res.sendPage("xml2/upload", null);
-    } else if (req.getCocoonRequest().getMethod().equalsIgnoreCase("POST")) {
-      String location = req.getCocoonRequest().getParameter("location");
-      String type = req.getCocoonRequest().getParameter("type");
-
-      ImportData importData = new ImportData();
-      importData.load(location, type);
-      res.sendPage("xml2/upload", null);
+    public void setSparqlDispatcher
+            (SparqlDispatcher
+                    sparqlDispatcher) {
+        this.sparqlDispatcher = sparqlDispatcher;
     }
-  }
 
-  public void setSparqlDispatcher
-          (SparqlDispatcher
-                  sparqlDispatcher) {
-    this.sparqlDispatcher = sparqlDispatcher;
-  }
-
-  public void setSparulDispatcher
-          (SparulDispatcher
-                  sparulDispatcher) {
-    this.sparulDispatcher = sparulDispatcher;
-  }
+    public void setSparulDispatcher
+            (SparulDispatcher
+                    sparulDispatcher) {
+        this.sparulDispatcher = sparulDispatcher;
+    }
 }
 
