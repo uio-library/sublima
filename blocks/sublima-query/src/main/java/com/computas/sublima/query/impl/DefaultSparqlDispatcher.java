20ae463df904e66adad51c142638a4a437e5d43e
kkj <kkj@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-05-14 16:00:47 +0000

    Debug statements in the dispatcher
    git-svn-id: https://submarine.computas.com/sublima/trunk@664 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparqlDispatcher.java
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparqlDispatcher.java
@@ -3,6 +3,7 @@ package com.computas.sublima.query.impl;
 import com.computas.sublima.query.SparqlDispatcher;
 import com.computas.sublima.query.service.SettingsService;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -13,6 +14,7 @@ import java.net.URLEncoder;
  * This component queries RDF triple stores using Sparql. It is threadsafe.
  */
 public class DefaultSparqlDispatcher implements SparqlDispatcher {
+    private static Logger logger = Logger.getLogger(DefaultSparqlDispatcher.class);
 
     /**
      * This method takes all SPARQL requests and identifies the corrent receiver.
@@ -29,9 +31,10 @@ public class DefaultSparqlDispatcher implements SparqlDispatcher {
             String url = SettingsService.getProperty("sublima.joseki.endpoint");
 
             URL u = new URL(url + "?query=" + URLEncoder.encode(query, "UTF-8"));
-
+            logger.debug("SPARQLdispatcher connected to Joseki.");
             con = (HttpURLConnection) u.openConnection();
             result = IOUtils.toString(con.getInputStream());
+            logger.info("SPARQLdispatcher got results from Joseki.");
         } catch (Exception e) {
             con.disconnect();
             e.printStackTrace();
