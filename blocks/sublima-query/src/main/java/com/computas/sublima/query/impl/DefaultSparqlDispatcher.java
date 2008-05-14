9399239bfbd2eb86d5d3470a9a94e5f10986fa35
kkj <kkj@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-05-14 15:56:01 +0000

    Send all queries to Joseki
    git-svn-id: https://submarine.computas.com/sublima/trunk@663 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparqlDispatcher.java
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparqlDispatcher.java
@@ -1,97 +1,44 @@
 package com.computas.sublima.query.impl;
 
+import com.computas.sublima.query.SparqlDispatcher;
+import com.computas.sublima.query.service.SettingsService;
+import org.apache.commons.io.IOUtils;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.sql.SQLException;
-import java.io.ByteArrayOutputStream;
-import java.util.Map;
-import java.util.HashMap;
 
-import org.apache.commons.io.IOUtils;
-
-import com.computas.sublima.query.SparqlDispatcher;
-import com.computas.sublima.query.service.SettingsService;
-import com.computas.sublima.query.service.DatabaseService;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.db.IDBConnection;
-import com.hp.hpl.jena.db.ModelRDB;
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
 
 /**
  * This component queries RDF triple stores using Sparql. It is threadsafe.
  */
 public class DefaultSparqlDispatcher implements SparqlDispatcher {
 
-  /**
-   * This method takes all SPARQL requests and identifies the corrent receiver.
-   * 
-   * @param query
-   * @return Object - based on what method it forwards to. Must be cased in cases
-   * where the returning object is not derived from java.lang.Object 
-   */
-  public Object query(String query) {
-
-    /*  Detect if the SPARQL should be sent to Joseki or LARQ
-     *  If the query contains the URI <http://jena.hpl.hp.com/ARQ/property#>
-     *  we know that it is a free text search
+    /**
+     * This method takes all SPARQL requests and identifies the corrent receiver.
+     *
+     * @param query
+     * @return Object - based on what method it forwards to. Must be cased in cases
+     *         where the returning object is not derived from java.lang.Object
      */
-    if(query.contains("<http://jena.hpl.hp.com/ARQ/property#>")) {
-      return freesearchQuery(query);
-    }
-    else { // It's a structured search (ie. advanced search)
-      return structuredQuery(query);
-    }
-	}
+    public Object query(String query) {
 
-  private Object structuredQuery(String query) {
-    String result = null;
-    HttpURLConnection con = null;
-    try {
-      String url = SettingsService.getProperty("sublima.joseki.endpoint");
+        String result = null;
+        HttpURLConnection con = null;
+        try {
+            String url = SettingsService.getProperty("sublima.joseki.endpoint");
 
-      URL u = new URL(url + "?query=" + URLEncoder.encode(query, "UTF-8"));
+            URL u = new URL(url + "?query=" + URLEncoder.encode(query, "UTF-8"));
 
-      con = (HttpURLConnection) u.openConnection();
-      result = IOUtils.toString(con.getInputStream());
-    } catch (Exception e) {
-      con.disconnect();
-      e.printStackTrace();
-    }
-    con.disconnect();
-    return result;
-  }
-
-  /**
-   * Method do perform a free text search on the index based on the DB model
-   * @param sparqlQuery - A String containing the SPARQL query
-   * @return queryResult - An RDF Model containing the result as a graph
-   */
-  public Object freesearchQuery(String sparqlQuery) {
-    DatabaseService myDbService = new DatabaseService();
-    IDBConnection connection = myDbService.getConnection();
-
-    //Create a model based on the one in the DB
-    ModelRDB model = ModelRDB.open(connection);
-    try {
-      connection.close();
-    } catch (SQLException e) {
-      model.close();
-      e.printStackTrace();
+            con = (HttpURLConnection) u.openConnection();
+            result = IOUtils.toString(con.getInputStream());
+        } catch (Exception e) {
+            con.disconnect();
+            e.printStackTrace();
+        }
+        con.disconnect();
+        return result;
     }
 
-    Query query = QueryFactory.create(sparqlQuery);
-    QueryExecution qExec = QueryExecutionFactory.create(query, model);
-    Model queryResult = qExec.execDescribe();
 
-    ByteArrayOutputStream bout = new ByteArrayOutputStream();
-    queryResult.write(bout, "RDF/XML-ABBREV");
-    queryResult.close();
-    qExec.close();
-    
-    return bout.toString();
-  }
 }
