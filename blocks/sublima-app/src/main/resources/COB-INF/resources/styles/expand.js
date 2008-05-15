a0680ee91dc1bba30943b254835b0215d437c76d
dn <dn@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-05-15 09:02:52 +0000

    styling and javascript for expanding div area
    git-svn-id: https://submarine.computas.com/sublima/trunk@666 8fc9d99b-3020-0410-9de1-dfa816a989ce

---
+++ blocks/sublima-app/src/main/resources/COB-INF/resources/styles/expand.js
@@ -0,0 +1,8 @@
+
+function showHide(elementid){
+if (document.getElementById(elementid).style.display == 'none'){
+document.getElementById(elementid).style.display = '';
+} else {
+document.getElementById(elementid).style.display = 'none';
+}
+} 
