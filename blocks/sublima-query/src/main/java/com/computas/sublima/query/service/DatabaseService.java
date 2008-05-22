4c9ce675c2dce3ed582bf8e1f44c5527c4129f2f
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-05-22 14:04:44 +0000

    git-svn-id: https://submarine.computas.com/sublima/trunk@704 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-query/src/main/java/com/computas/sublima/query/service/DatabaseService.java
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/service/DatabaseService.java
@@ -1,23 +1,21 @@
 package com.computas.sublima.query.service;
 
 
-import com.hp.hpl.jena.db.IDBConnection;
 import com.hp.hpl.jena.db.DBConnection;
+import com.hp.hpl.jena.db.IDBConnection;
 
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 /**
  * @author: mha
  * Date: 08.jan.2008
  */
 public class DatabaseService {
-   private String M_DB_URL = "jdbc:postgresql://localhost/subdata";
-   private String M_DB_USER = "subuser";
-   private String M_DB_PASSWD = "subpasswd";
-   private String M_DB = "PostgreSQL";
-   private String M_DBDRIVER_CLASS = "org.postgresql.Driver";
+  private String M_DB_URL = "jdbc:postgresql://localhost/subdata";
+  private String M_DB_USER = "subuser";
+  private String M_DB_PASSWD = "subpasswd";
+  private String M_DB = "PostgreSQL";
+  private String M_DBDRIVER_CLASS = "org.postgresql.Driver";
 
   public IDBConnection getConnection() {
 
@@ -39,13 +37,46 @@ public class DatabaseService {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-      Connection connection = null;
-      try {
-          connection = DriverManager.getConnection(M_DB_URL, M_DB_USER, M_DB_PASSWD);
-      } catch (SQLException e) {
-          e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-      }
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection(M_DB_URL, M_DB_USER, M_DB_PASSWD);
+    } catch (SQLException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    }
 
-      return connection;
+    return connection;
+  }
+
+  public int doSQLUpdate(String sql) throws SQLException {
+    Statement stmt;
+    int rows;
+    Connection con = getJavaSQLConnection();
+
+    stmt = con.createStatement();
+    rows = stmt.executeUpdate(sql);
+    con.commit();
+    stmt.close();
+    con.close();
+
+    return rows;
+  }
+
+  /**
+   * Method do do a SQL query. Returns a Statement instead of a ResultSet since a ResultSet is closed whenever the Statement is closed.
+   * Use Statement.getResultSet() to get the actual ResultSet.
+   * @param sql
+   * @return Statement
+   * @throws SQLException
+   */
+  public Statement doSQLQuery(String sql) throws SQLException {
+    Statement stmt;
+    ResultSet rs;
+    Connection con = getJavaSQLConnection();
+
+    stmt = con.createStatement();
+    rs = stmt.executeQuery(sql);
+    con.close();
+
+    return stmt;
   }
 }
