ad296bdf46a388efc94bfc8812138a952cdfd674
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-06-11 08:34:09 +0000

    git-svn-id: https://submarine.computas.com/sublima/trunk@799 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/LoginController.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.computas.sublima.app.controller;
 
 import com.computas.sublima.app.service.AdminService;
 import com.computas.sublima.query.service.DatabaseService;
+import com.computas.sublima.query.service.SettingsService;
 import org.apache.cocoon.auth.ApplicationManager;
 import org.apache.cocoon.auth.AuthenticationException;
 import org.apache.cocoon.auth.User;
@@ -77,7 +78,8 @@ public class LoginController extends AbstractSecurityHandler {
 
     // Get the user role and set it as an attribute
     if (name.equalsIgnoreCase("Computas")) {
-      user.setAttribute("role", "http://localhost:8180/sublima-webapp-1.0-SNAPSHOT/role/Computas");
+      String role = SettingsService.getProperty("sublima.base.url") + "role/Computas";
+      user.setAttribute("role", role);
     } else {
       user.setAttribute("role", adminService.getUserRole(name));
     }
