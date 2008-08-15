3a38dc3bcf6bed598c31a6b61aaf4eb2abcb1ad6
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-08-15 11:32:50 +0000

    git-svn-id: https://submarine.computas.com/sublima/trunk@1046 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/listener/ContextListener.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/listener/ContextListener.java
@@ -25,11 +25,15 @@ public final class ContextListener implements ServletContextListener {
     if ("true".equalsIgnoreCase(SettingsService.getProperty("sublima.index.internal.onstartup"))) {
       logger.info("SUBLIMA: Property sublima.index.internal.onstartup set to TRUE --> Indexing - Internal content");
       try {
-        indexService.updateResourceSearchfield(Boolean.valueOf(SettingsService.getProperty("sublima.index.external.onstartup")), SettingsService.getProperty("sublima.searchfields").split(";"), SettingsService.getProperty("sublima.prefixes").split(";"));
+
+        if ("true".equalsIgnoreCase(SettingsService.getProperty("sublima.index.updateliterals"))) {
+          indexService.updateResourceSearchfield(Boolean.valueOf(SettingsService.getProperty("sublima.index.external.onstartup")), SettingsService.getProperty("sublima.searchfields").split(";"), SettingsService.getProperty("sublima.prefixes").split(";"));
+        }
+        
         indexService.createIndex(SettingsService.getProperty("sublima.index.directory"), SettingsService.getProperty("sublima.index.type"));
         logger.info("SUBLIMA: Indexing - Internal content finished");
       } catch (com.hp.hpl.jena.shared.DoesNotExistException e) {
-        logger.warn("SUBLIMA: getFreetextToIndex() --> Database didn't exist. This is OK on startup, but fatal otherwise");
+        logger.warn("SUBLIMA: getFreetextToIndex() --> Database didn't exist. This iads OK on startup, but fatal otherwise");
       }
     }
   }
