54ff878cd94a0d8390e7052a7494b182ff0721d2
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-08-18 17:29:10 +0000

    Class to test the SHA1 implementation.
    git-svn-id: https://submarine.computas.com/sublima/trunk@1076 8fc9d99b-3020-0410-9de1-dfa816a989ce

---
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/adhoc/GenerateSHA1.java
@@ -0,0 +1,62 @@
+package com.computas.sublima.app.adhoc;
+
+import com.computas.sublima.app.service.AdminService;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author: mha
+ * Date: 18.aug.2008
+ */
+public class GenerateSHA1 {
+  AdminService adminService = new AdminService();
+
+  private static String convertToHex(byte[] data) {
+    StringBuffer buf = new StringBuffer();
+    for (int i = 0; i < data.length; i++) {
+      int halfbyte = (data[i] >>> 4) & 0x0F;
+      int two_halfs = 0;
+      do {
+        if ((0 <= halfbyte) && (halfbyte <= 9))
+          buf.append((char) ('0' + halfbyte));
+        else
+          buf.append((char) ('a' + (halfbyte - 10)));
+        halfbyte = data[i] & 0x0F;
+      } while (two_halfs++ < 1);
+    }
+    return buf.toString();
+  }
+
+  public static String generateSHA1(String text)
+          throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    MessageDigest md;
+    md = MessageDigest.getInstance("SHA-1");
+    byte[] sha1hash = new byte[40];
+    md.update(text.getBytes("UTF-8"), 0, text.length());
+    sha1hash = md.digest();
+    return convertToHex(sha1hash);
+  }
+
+  public static void main(String[] args) {
+
+    if (args.length == 0) {
+      System.err.println("A text to create the SHA1 sum for must be given.");
+      System.out.println("Usage: GenerateSha1 MyText");
+    } else {
+
+      String sha1;
+      try {
+        sha1 = generateSHA1(args[0]);
+      } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+        sha1 = "FAILED";
+      } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+        sha1 = "FAILED";
+      }
+      System.out.println("SHA1 for " + args[0] + ": " + sha1);
+    }
+  }
+}
