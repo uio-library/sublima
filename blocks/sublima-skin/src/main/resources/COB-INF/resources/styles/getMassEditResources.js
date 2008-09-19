4e8d1f9e0f44376a47a6272a8a7328d38a35fa55
kkj <kkj@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-09-19 12:01:13 +0000

    WTF
    git-svn-id: https://submarine.computas.com/sublima/trunk@1213 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-skin/src/main/resources/COB-INF/resources/styles/getMassEditResources.js
+++ blocks/sublima-skin/src/main/resources/COB-INF/resources/styles/getMassEditResources.js
@@ -1,6 +1,6 @@
 function getResources() {
   sparqlquery = 'PREFIX dct: <http://purl.org/dc/terms/>\nPREFIX sub: <http://xmlns.computas.com/sublima#>\nPREFIX xsd:    <http://www.w3.org/2001/XMLSchema#>\nSELECT ?uri ?title WHERE \n{ ?uri dct:title ?title ;\n   a sub:Resource  .\n   FILTER regex(xsd:string(?uri), "'+document.getElementById("selectregex").value+'", "i")\n} ORDER BY xsd:string(?uri)';
-  
+  $().ajaxStart($.blockUI).ajaxStop($.unblockUI);
   $.ajax({
     type: "GET",
     url: "../../sparql",
@@ -18,10 +18,10 @@ function createTable(data) {
   var regex = new RegExp(document.getElementById("selectregex").value, 'i');
   patternout = document.getElementById("selectpattern").value;
   $("#my-table tbody tr").remove();
-  
+  $("#resources-to-modify").removeAttr("style");
   for (var i = 0 ; i < data.results.bindings.length; i++) {
     oldurl = data.results.bindings[i].uri.value;
     newurl = oldurl.replace(regex, patternout);
-    $("#my-table tbody").append("<tr><td>"+ (i+1) +"</td><td>"+data.results.bindings[i].title.value+"</td><td><input type=\"text\" name=\"old-"+i+"\" size=\"40\" value=\""+oldurl+"\" readonly=\"readonly\"/></td><td><input type=\"text\"  size=\"40\" name=\new-"+i+"\" value=\""+newurl+"\"/></td></tr>");
+    $("#my-table tbody").append("<tr><td>"+ (i+1) +"</td><td>"+data.results.bindings[i].title.value+"</td><td><input type=\"text\" name=\"old-"+i+"\" size=\"40\" value=\""+oldurl+"\" readonly=\"readonly\"/></td><td><input type=\"text\"  size=\"40\" name=\"new-"+i+"\" value=\""+newurl+"\"/></td></tr>");
   }
 }
