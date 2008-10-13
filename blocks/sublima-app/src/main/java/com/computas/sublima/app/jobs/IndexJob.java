4ea6c6ac279d6fd9b480e8306eacf754732b200c
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-10-13 12:47:52 +0000

    git-svn-id: https://submarine.computas.com/sublima/trunk@1425 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/jobs/IndexJob.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/jobs/IndexJob.java
@@ -10,118 +10,74 @@ public class IndexJob {
 
   public static void main(String[] args) {
 
-    String usageString = "usage: IndexJob -indexExternalContent [true|false] -searchfields dct:title;dct:description -prefixes dct:<http://purl.org/dc/terms/;foaf:<http://xmlns.com/foaf/0.1/> -indextype [file|memory] [-indexdir directory]";
+    String usageString = "usage: IndexJob -searchfields dct:title;dct:description -prefixes dct:<http://purl.org/dc/terms/;foaf:<http://xmlns.com/foaf/0.1/> -indexdir /usr/local/index/ -indexexternal true|false";
 
-    boolean indexExternal = false;
     String[] searchfields = null;
-
-    /*
-      {"dct:title",
-       "dct:description",
-       "dct:publisher/foaf:name",
-       "dct:subject/skos:prefLabel",
-       "dct:subject/skos:altLabel",
-       "dct:subject/skos:hiddenLabel"};
-    */
-
     String[] prefixes = null;
-
-    /*
-      {"dct: <http://purl.org/dc/terms/>",
-       "foaf: <http://xmlns.com/foaf/0.1/>",
-       "sub: <http://xmlns.computas.com/sublima#>",
-       "skos: <http://www.w3.org/2004/02/skos/core#>"};
-    */
-
     String indexDir = ""; //"/tmp/june";
-    String indexType = ""; //"file";
+    String indexType = "file"; //"file";
+    boolean indexexternal = false;
 
     String argument;
 
     for (int i = 0; i < args.length; i++) {
       argument = args[i];
 
-      if (argument.equalsIgnoreCase("-indexExternalContent")) {
+      if (argument.equals("-searchfields")) {
         if (i < args.length + 1) {
-          if (args[i + 1].equalsIgnoreCase("true") || args[i + 1].equalsIgnoreCase("false")) {
-            indexExternal = Boolean.valueOf(args[i + 1]);
-          } else {
-            System.err.println("-indexExternalContent requires true or false");
-            System.err.println(usageString);
-            return;
-          }
-        }
-
-        if (argument.equals("-searchfields")) {
-          if (i < args.length + 1) {
-            searchfields = args[i + 1].split(";");
-          } else {
-            System.err.println("-searchfields requires a semicolon separated string of searchfields");
-            System.err.println(usageString);
-            return;
-          }
-        }
-
-
-        if (argument.equals("-prefixes")) {
-          if (i < args.length + 1) {
-            prefixes = args[i + 1].split(";");
-          } else {
-            System.err.println("-prefixes requires a semicolon separated string of prefixes");
-            System.err.println(usageString);
-            return;
-          }
-        }
-
-        if (argument.equals("-indextype")) {
-          if (i < args.length + 1) {
-            if (args[i + 1].equalsIgnoreCase("file") || args[i + 1].equalsIgnoreCase("memory")) {
-              indexType = args[i + 1];
-            } else {
-              System.err.println("-indextype requires file or memory");
-              System.err.println(usageString);
-              return;
-            }
-          }
-        }
-
-        if (argument.equals("-indextype")) {
-          if (i < args.length + 1) {
-            if (args[i + 1].equalsIgnoreCase("file") || args[i + 1].equalsIgnoreCase("memory")) {
-              indexType = args[i + 1];
-            } else {
-              System.err.println("-indextype requires file or memory");
-              System.err.println(usageString);
-              return;
-            }
-          }
-        }
-
-        if (argument.equals("-indexdir")) {
-          if (i < args.length + 1) {
-            indexDir = args[i + 1];
-          } else {
-            System.err.println("-indextype requires file or memory");
-            System.err.println(usageString);
-            return;
-          }
+          searchfields = args[i + 1].split(";");
+        } else {
+          System.err.println("-searchfields requires a semicolon separated string of searchfields");
+          System.err.println(usageString);
+          return;
         }
       }
 
-      if (searchfields == null || prefixes == null || indexType == null) {
-        System.err.println(usageString);
-        return;
+
+      if (argument.equals("-prefixes")) {
+        if (i < args.length + 1) {
+          prefixes = args[i + 1].split(";");
+        } else {
+          System.err.println("-prefixes requires a semicolon separated string of prefixes");
+          System.err.println(usageString);
+          return;
+        }
       }
 
-      if ("file".equalsIgnoreCase(indexType) && ("".equalsIgnoreCase(indexDir) || indexDir == null)) {
-        System.err.println("-indexdir requires a location when indextype is set to file");
-        System.err.println(usageString);
-        return;
+      if (argument.equals("-indexdir")) {
+        if (i < args.length + 1) {
+          indexDir = args[i + 1];
+        } else {
+          System.err.println("-indexdir required");
+          System.err.println(usageString);
+          return;
+        }
       }
 
-      IndexService indexService = new IndexService();
-
-      indexService.createIndex(indexDir, indexType, searchfields, prefixes);
+      if (argument.equals("-indexexternal")) {
+        if (i < args.length + 1) {
+          if ("true".equalsIgnoreCase(args[i + 1]) || "false".equalsIgnoreCase(args[i + 1])) {
+            indexexternal = Boolean.valueOf(args[i + 1]);
+          } else {
+            System.err.println("-indexexternal must be true or false");
+            System.err.println(usageString);
+            return;
+          }
+        } else {
+          System.err.println("-indexexternal must be true or false");
+          System.err.println(usageString);
+          return;
+        }
+      }
     }
+
+    if (searchfields == null || prefixes == null || indexDir == null) {
+      System.err.println(usageString);
+      return;
+    }
+
+    IndexService indexService = new IndexService();
+
+    indexService.createIndex(indexDir, indexType, searchfields, prefixes, indexexternal);
   }
 }
