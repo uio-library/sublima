1bae4a70dbf667e7ad0776df64b020b798e74dfa
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-10-20 13:15:59 +0000

    Clean up
    git-svn-id: https://submarine.computas.com/sublima/trunk@1544 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-query/src/main/java/com/computas/sublima/query/service/SettingsService.java
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/service/SettingsService.java
@@ -1,13 +1,12 @@
 package com.computas.sublima.query.service;
 
 import com.hp.hpl.jena.db.ModelRDB;
-import com.hp.hpl.jena.query.larq.IndexBuilderString;
 import com.hp.hpl.jena.query.larq.IndexBuilderNode;
+import com.hp.hpl.jena.query.larq.IndexBuilderString;
 import org.apache.cocoon.configuration.Settings;
+import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
-import org.apache.log4j.Logger;
-
 
 import java.io.File;
 
@@ -16,7 +15,7 @@ import java.io.File;
  *
  * @author: mha
  * Date: 13.mar.2008
- *
+ * <p/>
  * Change Note 05-09-2008 (dn): Added facilities for Node-based index
  */
 public class SettingsService {
@@ -27,6 +26,7 @@ public class SettingsService {
   static IndexBuilderNode indexBuilderNode = null;
 
   private static Logger logger = Logger.getLogger(SettingsService.class);
+
   private SettingsService() {
 
   }
@@ -52,14 +52,13 @@ public class SettingsService {
     }
     return larqBuilder;
   }
-  
-  
-  
-  /** 
+
+
+  /**
    * The method returns the index builder
-   * If the writer does not exist it looks for an existing index in 
+   * If the writer does not exist it looks for an existing index in
    * the given directory. If an index is not there, a new is created.
-   *  
+   *
    * @param dir
    * @returns a IndexBuilderNode
    */
@@ -70,35 +69,29 @@ public class SettingsService {
       if (dir == null) {
         indexBuilderNode = new IndexBuilderNode();
       } else {
-      
+
         // check if exist, if not or corrupt - create a new
         try {
-            //todo Might fail here if it is a write.lock()
-            IndexWriter iw = new IndexWriter(dir, new StandardAnalyzer());
-            indexBuilderNode = new IndexBuilderNode(iw);
+          //todo Might fail here if it is a write.lock()
+          IndexWriter iw = new IndexWriter(dir, new StandardAnalyzer());
+          indexBuilderNode = new IndexBuilderNode(iw);
         } catch (org.apache.lucene.index.CorruptIndexException cie) {
-            logger.warn("SUBLIMA: getIndexBuilderNode() --> Indexing - Failed to retrieve index, creating a new (empty in-memory index)");
-            indexBuilderNode = new IndexBuilderNode();
+          logger.warn("SUBLIMA: getIndexBuilderNode() --> Indexing - Failed to retrieve index, creating a new (empty in-memory index)");
+          indexBuilderNode = new IndexBuilderNode();
         } catch (org.apache.lucene.store.LockObtainFailedException lofe) {
-            logger.warn("SUBLIMA: getIndexBuilderNode() --> Indexing - Failed to optain lock for index, creating a new (empty in-memory index)");
-            lofe.printStackTrace(); 
-            indexBuilderNode = new IndexBuilderNode();
+          logger.warn("SUBLIMA: getIndexBuilderNode() --> Indexing - Failed to optain lock for index, creating a new (empty in-memory index)");
+          lofe.printStackTrace();
+          indexBuilderNode = new IndexBuilderNode();
         } catch (java.io.IOException ie) {
-           logger.warn("SUBLIMA: getIndexBuilderNode() --> Indexing - Failed to access index, creating a new (empty in-memory index)");
-            ie.printStackTrace(); 
-            indexBuilderNode = new IndexBuilderNode();
+          logger.warn("SUBLIMA: getIndexBuilderNode() --> Indexing - Failed to access index, creating a new (empty in-memory index)");
+          ie.printStackTrace();
+          indexBuilderNode = new IndexBuilderNode();
         }
       }
     }
     return indexBuilderNode;
   }
-  
-  
-  
-  
-  
-  
-  
+
 
   public void setCocoonSettings(Settings cocoonSettings) {
     this.cocoonSettings = cocoonSettings;
