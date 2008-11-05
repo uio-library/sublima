708d156d85f9e9bfb9e614a701fd6fbc80865aac
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2008-11-05 19:44:39 +0000

    OKE-480
    git-svn-id: https://submarine.computas.com/sublima/trunk@1756 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/SearchController.java
+++ blocks/sublima-app/src/main/java/com/computas/sublima/app/controller/SearchController.java
@@ -1,13 +1,14 @@
 package com.computas.sublima.app.controller;
 
-import com.computas.sublima.app.service.Form2SparqlService;
 import com.computas.sublima.app.service.AdminService;
+import com.computas.sublima.app.service.Form2SparqlService;
 import com.computas.sublima.query.SparqlDispatcher;
+import com.computas.sublima.query.service.CachingService;
 import com.computas.sublima.query.service.SearchService;
 import com.computas.sublima.query.service.SettingsService;
-import com.computas.sublima.query.service.CachingService;
 import static com.computas.sublima.query.service.SettingsService.getProperty;
 import com.hp.hpl.jena.sparql.util.StringUtils;
+import net.spy.memcached.MemcachedClient;
 import org.apache.cocoon.auth.ApplicationManager;
 import org.apache.cocoon.components.flow.apples.AppleRequest;
 import org.apache.cocoon.components.flow.apples.AppleResponse;
@@ -15,9 +16,10 @@ import org.apache.cocoon.components.flow.apples.StatelessAppleController;
 import org.apache.cocoon.environment.Request;
 import org.apache.log4j.Logger;
 
-import java.util.*;
-
-import net.spy.memcached.MemcachedClient;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SearchController implements StatelessAppleController {
   private SparqlDispatcher sparqlDispatcher;
@@ -42,24 +44,24 @@ public class SearchController implements StatelessAppleController {
 
     // The initial advanced search page
     if ("advancedsearch".equalsIgnoreCase(mode)) {
-        Map<String, Object> bizData = new HashMap<String, Object>();
-        bizData.put("loggedin", loggedIn);
-        if (loggedIn) {
-            bizData.put("statuses", adminService.getDistinctAndUsedLabels("<http://www.w3.org/2007/05/powder#DR>",
-                    "<http://www.w3.org/2007/05/powder#describedBy>"));
-        } else {
-            bizData.put("statuses", "<empty></empty>");
-        }
-        bizData.put("languages", adminService.getDistinctAndUsedLabels("<http://www.lingvoj.org/ontology#Lingvo>", "dct:language"));
-        bizData.put("mediatypes", adminService.getDistinctAndUsedLabels("dct:MediaType", "dct:format"));
-        bizData.put("audiences", adminService.getDistinctAndUsedLabels("dct:AgentClass", "dct:audience"));
-        bizData.put("committers", adminService.getDistinctAndUsedLabels("<http://rdfs.org/sioc/ns#User>", "<http://xmlns.computas.com/sublima#committer>"));
-        StringBuilder params = adminService.getMostOfTheRequestXML(req);
-        params.append("\n  </request>\n");
-        bizData.put("facets", params.toString());
-        System.gc();
-        res.sendPage("xml/advancedsearch", bizData);
-        return;
+      Map<String, Object> bizData = new HashMap<String, Object>();
+      bizData.put("loggedin", loggedIn);
+      if (loggedIn) {
+        bizData.put("statuses", adminService.getDistinctAndUsedLabels("<http://www.w3.org/2007/05/powder#DR>",
+                "<http://www.w3.org/2007/05/powder#describedBy>"));
+      } else {
+        bizData.put("statuses", "<empty></empty>");
+      }
+      bizData.put("languages", adminService.getDistinctAndUsedLabels("<http://www.lingvoj.org/ontology#Lingvo>", "dct:language"));
+      bizData.put("mediatypes", adminService.getDistinctAndUsedLabels("dct:MediaType", "dct:format"));
+      bizData.put("audiences", adminService.getDistinctAndUsedLabels("dct:AgentClass", "dct:audience"));
+      bizData.put("committers", adminService.getDistinctAndUsedLabels("<http://rdfs.org/sioc/ns#User>", "<http://xmlns.computas.com/sublima#committer>"));
+      StringBuilder params = adminService.getMostOfTheRequestXML(req);
+      params.append("\n  </request>\n");
+      bizData.put("facets", params.toString());
+      System.gc();
+      res.sendPage("xml/advancedsearch", bizData);
+      return;
     }
 
     // If it's search-results for advanced search, topic instance or resource
@@ -82,7 +84,7 @@ public class SearchController implements StatelessAppleController {
             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
             "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>",
             "PREFIX wdr: <http://www.w3.org/2007/05/powder#>",
-                    "DESCRIBE ?resource " + subject + " ?publisher ?subjects ?rest",
+            "DESCRIBE ?resource " + subject + " ?publisher ?subjects ?rest",
             "WHERE {",
             "        ?resource dct:language ?lang;",
             "				 dct:publisher ?publisher ;",
@@ -99,48 +101,45 @@ public class SearchController implements StatelessAppleController {
     // This query, relies on that all relations are explicitly stated.
     // I.e. a triple for both skos:broader and skos:narrower must exist.
     // Small fix added allowing concepts to not have relations at all.
-     String sparqlConstructQuery =
-        "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n"+
-        "prefix skos: <http://www.w3.org/2004/02/skos/core#> \n"+
-        "prefix owl: <http://www.w3.org/2002/07/owl#> \n"+
-        "CONSTRUCT {\n"+
-         subject + "\n" +
-        "   a skos:Concept ;\n"+
-        "   skos:prefLabel ?preflabel ; \n"+
-        "   skos:altLabel ?altlabel ;  \n"+
-        "   skos:definition ?definition ; \n" +
-        "   ?semrelation ?object . \n"+
-        "?semrelation rdfs:subPropertyOf skos:semanticRelation ;\n"+
-        "   rdfs:label ?semrellabel ;\n"+
-        "   a owl:ObjectProperty .\n"+       
-        "?object skos:prefLabel ?preflabel2 ; \n"+
-        "   a skos:Concept .\n"+
-        "}\n"+
-        "WHERE {\n"+ 
-         subject + "\n" + 
-         "   skos:prefLabel ?preflabel ;\n"+
-         "   a skos:Concept .\n"+
-         "OPTIONAL {\n"+
-         subject + "\n" + 
-         "   skos:altLabel ?altlabel .\n" +
-         "}\n"+
-         "OPTIONAL {\n"+
-         subject + "\n" +
-         "   skos:definition ?definition ;\n" +
-         "}\n"+
-         "OPTIONAL {\n" +
-         subject + "\n" + 
-         "   ?semrelation ?object .\n"+
-         "?semrelation rdfs:subPropertyOf skos:semanticRelation ;\n"+
-         "   rdfs:label ?semrellabel ;\n"+
-         "   a owl:ObjectProperty .\n"+       
-         "?object  a skos:Concept ;\n"+
-         "   skos:prefLabel ?preflabel2 .\n"+
-         "}\n"+ 
-         "}";
-
-
-
+    String sparqlConstructQuery =
+            "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" +
+                    "prefix skos: <http://www.w3.org/2004/02/skos/core#> \n" +
+                    "prefix owl: <http://www.w3.org/2002/07/owl#> \n" +
+                    "CONSTRUCT {\n" +
+                    subject + "\n" +
+                    "   a skos:Concept ;\n" +
+                    "   skos:prefLabel ?preflabel ; \n" +
+                    "   skos:altLabel ?altlabel ;  \n" +
+                    "   skos:definition ?definition ; \n" +
+                    "   ?semrelation ?object . \n" +
+                    "?semrelation rdfs:subPropertyOf skos:semanticRelation ;\n" +
+                    "   rdfs:label ?semrellabel ;\n" +
+                    "   a owl:ObjectProperty .\n" +
+                    "?object skos:prefLabel ?preflabel2 ; \n" +
+                    "   a skos:Concept .\n" +
+                    "}\n" +
+                    "WHERE {\n" +
+                    subject + "\n" +
+                    "   skos:prefLabel ?preflabel ;\n" +
+                    "   a skos:Concept .\n" +
+                    "OPTIONAL {\n" +
+                    subject + "\n" +
+                    "   skos:altLabel ?altlabel .\n" +
+                    "}\n" +
+                    "OPTIONAL {\n" +
+                    subject + "\n" +
+                    "   skos:definition ?definition ;\n" +
+                    "}\n" +
+                    "OPTIONAL {\n" +
+                    subject + "\n" +
+                    "   ?semrelation ?object .\n" +
+                    "?semrelation rdfs:subPropertyOf skos:semanticRelation ;\n" +
+                    "   rdfs:label ?semrellabel ;\n" +
+                    "   a owl:ObjectProperty .\n" +
+                    "?object  a skos:Concept ;\n" +
+                    "   skos:prefLabel ?preflabel2 .\n" +
+                    "}\n" +
+                    "}";
 
     //logger.trace("doGetTopic: SPARQL CONSTRUCT query sent to dispatcher:\n" + sparqlConstructQuery);
     queryResult = sparqlDispatcher.query(sparqlConstructQuery);
@@ -212,7 +211,7 @@ public class SearchController implements StatelessAppleController {
     StringBuilder xmlSearchParametersBuffer = new StringBuilder();
     xmlSearchParametersBuffer.append("<c:searchparams xmlns:c=\"http://xmlns.computas.com/cocoon\">\n");
     if (req.getCocoonRequest().getParameter("searchstring") == null) {
-      xmlSearchParametersBuffer.append("\t<c:searchstring></c:searchstring>\n");      
+      xmlSearchParametersBuffer.append("\t<c:searchstring></c:searchstring>\n");
     } else {
       String searchstring = req.getCocoonRequest().getParameter("searchstring");
       searchstring = searchstring.replace("&", "&amp;");
@@ -220,7 +219,7 @@ public class SearchController implements StatelessAppleController {
       searchstring = searchstring.replace(">", "&gt;");
       xmlSearchParametersBuffer.append("\t<c:searchstring>" + searchstring + "</c:searchstring>\n");
     }
-    
+
     xmlSearchParametersBuffer.append("\t<c:operator>" + req.getCocoonRequest().getParameter("booleanoperator") + "</c:operator>\n");
     xmlSearchParametersBuffer.append("\t<c:deepsearch>" + req.getCocoonRequest().getParameter("deepsearch") + "</c:deepsearch>\n");
     xmlSearchParametersBuffer.append("\t<c:sortby>" + req.getCocoonRequest().getParameter("sort") + "</c:sortby>\n");
@@ -270,58 +269,61 @@ public class SearchController implements StatelessAppleController {
 
     Object queryResult;
     if (doBigSearchAnyway) {
-        // This will do the search despite it being large, thus populating the cache
-        queryResult = sparqlDispatcher.query(sparqlQuery);
+      // This will do the search despite it being large, thus populating the cache
+      queryResult = sparqlDispatcher.query(sparqlQuery);
     } else if (doSearch) {
-        if (adminService.isAboveMaxNumberOfHits(countNumberOfHitsQuery)) {
-            // We are above the threshold, lets see if we have it cached
-            CachingService cache = new CachingService();
-            MemcachedClient memcached = cache.connect();
+      if (adminService.isAboveMaxNumberOfHits(countNumberOfHitsQuery)) {
+        // We are above the threshold, lets see if we have it cached
+        CachingService cache = new CachingService();
+        MemcachedClient memcached = cache.connect();
 
-            String cacheString = sparqlQuery.replaceAll("\\s+", " ") + SettingsService.getProperty("sublima.base.url");
-            String cacheKey = String.valueOf(cacheString.hashCode()); // We could parse the query first to get a better key
-            //  logger.trace("SPARQLdispatcher hashing for use as key.\n" + cacheString + "\n");
-            if (cache.ask(memcached, cacheKey)) {
-                logger.debug("SearchController found the query in the cache.");
-                queryResult = sparqlDispatcher.query(sparqlQuery); // Cache will be used in here.
-            } else {
-                queryResult = "<empty/>";
-            }
-        } else {
-            // We are below the threshold, do the search. Cache will be checked
-            queryResult = sparqlDispatcher.query(sparqlQuery);
+        String cacheString = sparqlQuery.replaceAll("\\s+", " ") + SettingsService.getProperty("sublima.base.url");
+        String cacheKey = String.valueOf(cacheString.hashCode()); // We could parse the query first to get a better key
+        //  logger.trace("SPARQLdispatcher hashing for use as key.\n" + cacheString + "\n");
+        try {
+          if (cache.ask(memcached, cacheKey)) {
+            logger.debug("SearchController found the query in the cache.");
+            queryResult = sparqlDispatcher.query(sparqlQuery); // Cache will be used in here.
+          } else {
+            queryResult = "<empty/>";
+          }
+        } catch (Exception e) {
+          queryResult = "<empty/>";
         }
-    }
-    else {
-        queryResult = "<empty/>";
+      } else {
+        // We are below the threshold, do the search. Cache will be checked
+        queryResult = sparqlDispatcher.query(sparqlQuery);
+      }
+    } else {
+      queryResult = "<empty/>";
     }
 
 
     if (searchStringOverriden == null) {
-        searchStringOverriden = req.getCocoonRequest().getParameter("dct:subject/all-labels");
+      searchStringOverriden = req.getCocoonRequest().getParameter("dct:subject/all-labels");
     }
 
     if (searchStringOverriden == null) {
-        searchStringOverriden = req.getCocoonRequest().getParameter("dct:subject/skos:prefLabel");
+      searchStringOverriden = req.getCocoonRequest().getParameter("dct:subject/skos:prefLabel");
     }
 
     Object navigationResults = "<empty></empty>";
     if (searchStringOverriden != null && searchStringOverriden.length() > 0) {
-        // Now get the matching topics
-        String sparqlTopicsQuery =
-                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n"+
-                "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n"+
-                "PREFIX owl: <http://www.w3.org/2002/07/owl#> \n"+
-                "PREFIX wdr: <http://www.w3.org/2007/05/powder#>\n" +
-                "PREFIX pf: <http://jena.hpl.hp.com/ARQ/property#> \n" +
-                "DESCRIBE ?subject WHERE {\n"+
-                "  ?lit pf:textMatch \"\"\"" + searchStringOverriden + "\"\"\" . " +
-                "  ?subject skos:prefLabel ?lit .\n" +
-                "  ?subject wdr:describedBy <http://sublima.computas.com/status/godkjent_av_administrator> . \n" +                        
-                "\n}\n";
-        
-        //logger.trace("doAdvanced: SPARQL query to get topics sent to dispatcher:\n" + sparqlTopicsQuery);
-        navigationResults = sparqlDispatcher.query(sparqlTopicsQuery);
+      // Now get the matching topics
+      String sparqlTopicsQuery =
+              "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" +
+                      "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n" +
+                      "PREFIX owl: <http://www.w3.org/2002/07/owl#> \n" +
+                      "PREFIX wdr: <http://www.w3.org/2007/05/powder#>\n" +
+                      "PREFIX pf: <http://jena.hpl.hp.com/ARQ/property#> \n" +
+                      "DESCRIBE ?subject WHERE {\n" +
+                      "  ?lit pf:textMatch \"\"\"" + searchStringOverriden + "\"\"\" . " +
+                      "  ?subject skos:prefLabel ?lit .\n" +
+                      "  ?subject wdr:describedBy <http://sublima.computas.com/status/godkjent_av_administrator> . \n" +
+                      "\n}\n";
+
+      //logger.trace("doAdvanced: SPARQL query to get topics sent to dispatcher:\n" + sparqlTopicsQuery);
+      navigationResults = sparqlDispatcher.query(sparqlTopicsQuery);
     }
 
     bizData.put("result-list", queryResult);
@@ -338,9 +340,8 @@ public class SearchController implements StatelessAppleController {
     res.sendPage(format + "/sparql-result", bizData);
   }
 
-  
 
-    private Map<String, String[]> createParametersMap(Request request) {
+  private Map<String, String[]> createParametersMap(Request request) {
     Map<String, String[]> result = new HashMap<String, String[]>();
     Enumeration parameterNames = request.getParameterNames();
     while (parameterNames.hasMoreElements()) {
