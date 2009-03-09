5fb7ab7f0edae7bbb0f3bdcad285009c46fd28fb
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2009-03-09 12:31:14 +0000

    Modified the SPARUL dispatcher for Virtuoso, and added the authentication credentials to the properties file.
    git-svn-id: https://submarine.computas.com/sublima/trunk@2661 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparulDispatcher.java
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/impl/DefaultSparulDispatcher.java
@@ -9,67 +9,97 @@ import com.hp.hpl.jena.sparql.ARQNotImplemented;
 import com.hp.hpl.jena.update.*;
 import org.apache.cocoon.configuration.Settings;
 import org.apache.log4j.Logger;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthPolicy;
+import org.apache.commons.httpclient.auth.AuthScope;
 import net.spy.memcached.MemcachedClient;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * This component queries RDF triple stores using Sparul. It is threadsafe.
  */
 public class DefaultSparulDispatcher implements SparulDispatcher {
 
-  private Settings cocoonSettings;
   private static Logger logger = Logger.getLogger(DefaultSparulDispatcher.class);
 
-  public boolean query(String query) {
+    /**
+     * Connects and authenticates with the SPARUL endpoint and sends the SPARUL with HTTP POST.
+     *
+     * @param query SPARQL/Update
+     * @return success true/false
+     */
+    public boolean query(String query) {
 
-    long connecttime = System.currentTimeMillis();
-    //Get a GraphStore and load the graph from the Model
-    GraphStore graphStore = GraphStoreFactory.create();
-    graphStore.setDefaultGraph(SettingsService.getModel().getGraph());
+      boolean success = false;
+      PostMethod postMethod = null;
 
-    try {
-      //Try to execute the updateQuery (SPARQL/Update)
-      logger.info("SPARULdispatcher executing.\n" + query + "\n");
-      UpdateRequest updateRequest = UpdateFactory.create(query);
-      updateRequest.exec(graphStore);
-      LARQ.setDefaultIndex(SettingsService.getIndexBuilderNode(null).getIndex());
-    } catch (ARQNotImplemented e) {
-      logger.warn("DefaultSparulDispatcher.query --> ARQNotImplemented exception. Returning TRUE and flagging reindexing. " + e.getMessage());
-      logger.info("SPARULdispatcher executing again because of exception.\n" + query + "\n");
-      UpdateRequest updateRequest = UpdateFactory.create(query);
+      //todo Put this in JNDI or what?
+      final String username = SettingsService.getProperty("virtuoso.digest.username");
+      final String password = SettingsService.getProperty("virtuoso.digest.password");
+      final String realm = SettingsService.getProperty("virtuoso.digest.realm");
 
-      updateRequest.exec(graphStore);
-      LARQ.setDefaultIndex(SettingsService.getIndexBuilderNode(null).getIndex());
-      return true;
-    } catch (UpdateException e) {
-      logger.warn("DefaultSparulDispatcher.query --> UpdateException. Returning FALSE. " + e.getMessage());
-      e.printStackTrace();
-      return false;
-    } catch (ARQLuceneException e) {
-      logger.warn("DefaultSparulDispatcher.query --> ARQLuceneException. Returning FALSE. " + e.getMessage());
-      e.printStackTrace();
-      return false;
-    }catch (Exception e) {
-      logger.warn("DefaultSparulDispatcher.query --> Exception. Returning FALSE. " + e.getMessage());
-      e.printStackTrace();
-      return false;
+      String url = SettingsService.getProperty("sublima.sparul.endpoint");
+      logger.info("SPARQLdispatcher executing.\n" + query + "\n");
+
+      try {
+
+        URL u = new URL(url);
+
+        HttpClient httpClient = new HttpClient();
+        List<String> authPrefs = new ArrayList<String>(1);
+        authPrefs.add(AuthPolicy.DIGEST);
+        httpClient.getParams().setParameter(AuthPolicy.AUTH_SCHEME_PRIORITY, authPrefs);
+
+        Credentials credentials = new UsernamePasswordCredentials(username, password);
+
+        AuthScope authScope = new AuthScope(u.getHost(), u.getPort(), realm);
+        httpClient.getState().setCredentials(authScope, credentials);
+
+        postMethod = new PostMethod(u.toExternalForm());
+        postMethod.setDoAuthentication(true);
+        postMethod.addParameter("query", query);
+
+        int result = httpClient.executeMethod(postMethod);
+
+        if (result != 200) {
+          logger.error("SPARULDispatcher: Error when trying to SPARUL.\n Query:\n" + query + "\n" + result + postMethod.getStatusText());
+          System.out.println(postMethod.getResponseBodyAsString());
+        } else { // Read the server response and return false if the response contains the word "error". Kinda shaky, but but... ;)
+          success = true;
+          BufferedReader input = new BufferedReader(new InputStreamReader(postMethod.getResponseBodyAsStream()));
+          String str;
+          while (null != ((str = input.readLine()))) {
+            if (str.contains("error")) {
+              success = false;
+            }
+          }
+          input.close();
+          CachingService cache = new CachingService();
+          MemcachedClient memcached = cache.connect();
+          cache.modelChanged(memcached);
+          cache.close(memcached);
+        }
+
+      } catch (Exception e) {
+        logger.error("SPARULDispatcher: Error when trying to SPARUL.\n Query:\n" + query + "\n" + postMethod.getStatusText());
+        e.printStackTrace();
+      }
+      finally {
+        if (postMethod != null) {
+          postMethod.releaseConnection();
+        }
+      }
+
+      return success;
     }
-
-    logger.debug("DefaultSparulDispatcher invalidates the cache.");  
-    CachingService cache = new CachingService();
-    MemcachedClient memcached = cache.connect();
-    cache.modelChanged(memcached);
-    cache.close(memcached);
-    System.gc();
-
-    long requesttime = System.currentTimeMillis() - connecttime;
-    logger.info("SPARULdispatcher executed. SPARUL took " + requesttime + " ms." );
-
-    // Return true if update success
-    return true;
-  }
-
-  public void setCocoonSettings(Settings cocoonSettings) {
-    this.cocoonSettings = cocoonSettings;
-  }
+  
 
 }
