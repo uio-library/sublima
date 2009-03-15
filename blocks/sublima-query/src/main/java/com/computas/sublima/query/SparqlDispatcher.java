c04f1c13832d54f294347feffc23155c3e0fd06a
mha <mha@8fc9d99b-3020-0410-9de1-dfa816a989ce>
2009-03-15 19:42:21 +0000

    Removed method that returned result set
    git-svn-id: https://submarine.computas.com/sublima/trunk@2743 8fc9d99b-3020-0410-9de1-dfa816a989ce

--- blocks/sublima-query/src/main/java/com/computas/sublima/query/SparqlDispatcher.java
+++ blocks/sublima-query/src/main/java/com/computas/sublima/query/SparqlDispatcher.java
@@ -1,17 +1,12 @@
 package com.computas.sublima.query;
 
-import com.hp.hpl.jena.query.ResultSet;
-
 public interface SparqlDispatcher {
-	
-	// FIXME In order to increase the peformance, the 
-	// sparql dispatcher shouldn't return a string object	
-	Object query(String sparqlQuery);
 
-    Object query(String sparqlQuery, String queryType);
+  // FIXME In order to increase the peformance, the
+  // sparql dispatcher shouldn't return a string object
+  Object query(String sparqlQuery);
+
+  Object query(String sparqlQuery, String queryType);
 
   String getResultsAsJSON(String sparqlQuery);
-
-  ResultSet getResultsAsResultSet(String sparqlQuery);
-
 }
